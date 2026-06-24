8ce9caf24eff84594cc14ae04fee46525ce2581d
RixonC <44545122+RixonC@users.noreply.github.com>
2026-06-24 18:26:14 +1000

    Update prepare_wild_places.sh

--- scripts/prepare_wild_places.sh
+++ scripts/prepare_wild_places.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-mkdir -p Datasets/Processed
+mkdir -p datasets/processed
 
+export JAX_PLATFORMS="cpu"
 export JAX_DEFAULT_MATMUL_PRECISION="highest"
-
-# Ensure the Python virtual environment is active.
 
 # Process sequences K-03, K-04, V-03, and V-04.
 
-python -u process_wild_places.py --save_path Datasets/Processed/wild_places_k_03.hdf5 --sequence K-03
-python -u process_wild_places.py --save_path Datasets/Processed/wild_places_k_04.hdf5 --sequence K-04
-python -u process_wild_places.py --save_path Datasets/Processed/wild_places_v_03.hdf5 --sequence V-03
-python -u process_wild_places.py --save_path Datasets/Processed/wild_places_v_04.hdf5 --sequence V-04
+uv run python -u data_preprocessing/process_wild_places.py --save_path datasets/processed/wild_places_k_03.hdf5 --sequence K-03
+uv run python -u data_preprocessing/process_wild_places.py --save_path datasets/processed/wild_places_k_04.hdf5 --sequence K-04
+uv run python -u data_preprocessing/process_wild_places.py --save_path datasets/processed/wild_places_v_03.hdf5 --sequence V-03
+uv run python -u data_preprocessing/process_wild_places.py --save_path datasets/processed/wild_places_v_04.hdf5 --sequence V-04
