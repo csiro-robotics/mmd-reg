d0d8f52efbda2c9c2acf5b824fba5ccf98c2c092
RixonC <44545122+RixonC@users.noreply.github.com>
2026-06-25 11:58:15 +1000

    Update prepare_kitti_odometry.sh

--- scripts/prepare_kitti_odometry.sh
+++ scripts/prepare_kitti_odometry.sh
@@ -7,7 +7,7 @@ export JAX_DEFAULT_MATMUL_PRECISION="highest"
 
 # Process sequences 07, 08, 09 and 10.
 
-uv run python -u data_preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_07.hdf5 --sequence 07
-uv run python -u data_preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_08.hdf5 --sequence 08
-uv run python -u data_preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_09.hdf5 --sequence 09
-uv run python -u data_preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_10.hdf5 --sequence 10
+uv run python -u preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_07.hdf5 --sequence 07
+uv run python -u preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_08.hdf5 --sequence 08
+uv run python -u preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_09.hdf5 --sequence 09
+uv run python -u preprocessing/process_kitti_odometry.py --save_path datasets/processed/kitti_odometry_10.hdf5 --sequence 10
