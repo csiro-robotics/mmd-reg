f006d7ab8ae4be6b3649c00b095a363c254ac02a
RixonC <44545122+RixonC@users.noreply.github.com>
2026-07-10 17:48:36 +1000

    Update benchmark_cpu_pcpnet.sh

--- scripts/benchmark_cpu_pcpnet.sh
+++ scripts/benchmark_cpu_pcpnet.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir -p results
-S="results/pcpnet_cpu.json"
 
 export PYTHONPATH=.
 export JAX_PLATFORMS="cpu"
@@ -13,6 +12,7 @@ export JAX_SKIP_CUDA_CONSTRAINTS_CHECK=1
 # Benchmark split for high noise.
 
 D="datasets/processed/pcpnet_high_noise.hdf5"
+S="results/pcpnet_cpu_high_noise.json"
 
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 16 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 32 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
@@ -28,6 +28,7 @@ uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --al
 # Benchmark split for gradient sampling density.
 
 D="datasets/processed/pcpnet_gradient.hdf5"
+S="results/pcpnet_cpu_gradient.json"
 
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 16 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 32 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
@@ -43,6 +44,7 @@ uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --al
 # Benchmark split for striped sampling density.
 
 D="datasets/processed/pcpnet_striped.hdf5"
+S="results/pcpnet_cpu_striped.json"
 
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 16 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm MMD-Reg --mmd_reg_Ds 32 --mmd_reg_ls 0.75 --mmd_reg_dist Gaussian
@@ -56,6 +58,8 @@ uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --al
 uv run python -u experiments/benchmark.py --data_path "$D" --save_path "$S" --algorithm SVR
 
 # Benchmark splits to measure CPU runtime for different numbers of points.
+
+S="results/pcpnet_cpu_time.json"
 
 for i in 1 2 4 6 8 10 20 30 40 50; do
     I=$(printf "%02d" "$i")
@@ -77,6 +81,8 @@ for i in 1 2 4 6 8 10; do
 done
 
 # Benchmark splits for different outlier percentages.
+
+S="results/pcpnet_cpu_outliers.json"
 
 for i in 0 1 2 10 20 30 40 50 60 70 80 90 100; do
     I=$(printf "%03d" "$i")
