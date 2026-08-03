d7ea40de7ef68a56823112eb8156bc5c5e81fd24
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-03 16:08:55 +1000

    Create plot_pcpnet_results.sh

---
+++ scripts/plot_pcpnet_results.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p results/figures
+
+export PYTHONPATH=.
+export JAX_PLATFORMS="cpu"
+export JAX_DEFAULT_MATMUL_PRECISION="highest"
+export JAX_SKIP_CUDA_CONSTRAINTS_CHECK=1
+
+uv run python -u experiments/plot_pcpnet_examples.py
+uv run python -u experiments/plot_pcpnet_results.py
