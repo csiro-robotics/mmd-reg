be55318df77cb1f38a13047db2b26d3e1b74efbe
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-14 16:09:05 +1000

    Update train_supervised.sh

--- scripts/train_supervised.sh
+++ scripts/train_supervised.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 mkdir -p results
+S="results/params_supervised_trained.msgpack"
+
+if [[ -f "$S" ]]; then
+	echo "Skipping supervised training as '$S' already exists."
+	exit 0
+fi
 
 export PYTHONPATH=.
 
