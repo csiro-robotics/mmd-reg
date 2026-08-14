b1d7504e80835b8d040449e1d20d5403afe5571e
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-14 16:08:13 +1000

    Update train_unsupervised_laplace.sh

--- scripts/train_unsupervised_laplace.sh
+++ scripts/train_unsupervised_laplace.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 mkdir -p results
+S="results/params_unsupervised_laplace_trained.msgpack"
+
+if [[ -f "$S" ]]; then
+	echo "Skipping unsupervised Laplace training as '$S' already exists."
+	exit 0
+fi
 
 export PYTHONPATH=.
 
