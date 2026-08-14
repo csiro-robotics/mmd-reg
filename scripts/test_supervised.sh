5dced43ea8e2aced025cf082e63118cbe6d51215
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-14 16:27:44 +1000

    Update test_supervised.sh

--- scripts/test_supervised.sh
+++ scripts/test_supervised.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+T="results/params_supervised_tuned.msgpack"
+
+if [[ ! -f "$T" ]]; then
+	echo "Skipping supervised testing as '$T' does not exist."
+	exit 0
+fi
+
 export PYTHONPATH=.
 export JAX_DEFAULT_MATMUL_PRECISION="bfloat16"
 
