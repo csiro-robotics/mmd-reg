fe135ff627f4d8fdd8e9681801e2d338a2a7ede8
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-14 16:10:26 +1000

    Update tune_supervised.sh

--- scripts/tune_supervised.sh
+++ scripts/tune_supervised.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+S="results/params_supervised_tuned.msgpack"
+
+if [[ -f "$S" ]]; then
+	echo "Skipping supervised tuning as '$S' already exists."
+	exit 0
+fi
+
 export PYTHONPATH=.
 
 uv run python -u experiments/tune_supervised.py
