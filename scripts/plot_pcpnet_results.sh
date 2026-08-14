addba85a8c2ecb42c73feb1639c5c87f89d27943
RixonC <44545122+RixonC@users.noreply.github.com>
2026-08-14 23:09:17 +1000

    Update plot_pcpnet_results.sh

--- scripts/plot_pcpnet_results.sh
+++ scripts/plot_pcpnet_results.sh
@@ -7,5 +7,4 @@ export JAX_PLATFORMS="cpu"
 export JAX_DEFAULT_MATMUL_PRECISION="highest"
 export JAX_SKIP_CUDA_CONSTRAINTS_CHECK=1
 
-uv run python -u experiments/plot_pcpnet_examples.py
 uv run python -u experiments/plot_pcpnet_results.py
