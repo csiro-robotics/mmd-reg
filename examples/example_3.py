37f500dc4c0488ef3cc0d9178cbebac4a49a3e94
RixonC <44545122+RixonC@users.noreply.github.com>
2026-09-25 17:22:35 +1000

    Create example_3.py

---
+++ examples/example_3.py
@@ -0,0 +1,35 @@
+import jax
+from mmd_reg import mmd_reg
+from example_utilities import draw_point_clouds, get_point_clouds_real
+
+jax.config.update("jax_default_matmul_precision", "highest")
+
+
+def main():
+    """Run MMD-Reg on a pair of point clouds and visualize the result."""
+    # Get source point cloud X and target point cloud Y.
+    X, Y = get_point_clouds_real()
+
+    # Visualize point clouds. The view is interactive.
+    draw_point_clouds(X, Y)
+
+    # Get predicted registration solution from MMD-Reg.
+    # Take care if timing MMD-Reg because JAX uses asynchronous dispatch.
+    # MMD-Reg is faster after JIT compilation for the same input shapes.
+    mmd_reg_jit = jax.jit(mmd_reg)
+    Ds = [256, 256, 256, 256, 256]
+    ls = [4.0, 2.0, 1.0, 0.5, 0.25]
+    Ws = []
+    key = jax.random.key(0)
+    for D, l in zip(Ds, ls):
+        key, W_key = jax.random.split(key, num=2)
+        W = jax.random.laplace(W_key, (D, 3)) / l  # Can also use normal.
+        Ws.append(W)
+    pred_R, pred_t = mmd_reg_jit(Ws, X, Y)
+
+    # Visualize predicted registration. The view is interactive.
+    draw_point_clouds(X @ pred_R.T + pred_t, Y)
+
+
+if __name__ == "__main__":
+    main()
